a86d99c55069b567168b1ca5826ca6604a65c707
kottipranith <saikotti2@gmail.com>
2024-12-24 19:34:07 +0530

    added the CanteenController

--- CantitoBackend/src/main/java/com/cantito/CantitoBackend/CantitoBackendApplication.java
+++ CantitoBackend/src/main/java/com/cantito/CantitoBackend/CantitoBackendApplication.java
@@ -18,28 +18,28 @@ public class CantitoBackendApplication {
 
 	public static void main(String[] args) {
 		ConfigurableApplicationContext context = SpringApplication.run(CantitoBackendApplication.class, args);
-		CanteenService canteenService = context.getBean(CanteenService.class);
+//		CanteenService canteenService = context.getBean(CanteenService.class);
 //		Canteen canteen = new Canteen();
 //		canteen.setCanteenName("sri amma mess");
 //		canteen.setLocation("c block 2nd floor beside washrooms");
 //		canteen.setOwnerId("pranith03");
 //		canteenService.save(canteen);
-
-		ItemService itemService = context.getBean(ItemService.class);
+//
+//		ItemService itemService = context.getBean(ItemService.class);
 //		Item item = new Item();
 //		item.setPrice(100);
 //		item.setAvaliable(true);
 //		item.setItemName("meals");
 //		itemService.save(1, item);
-		System.out.println(itemService.findById(1, 2));
-		System.out.println(itemService.findByCanteenId(1));
-		itemService.makeItemUnavailable(1, 2);
-		System.out.println(itemService.findById(1, 2));
-		itemService.makeItemAvailable(1, 2);
-		System.out.println(itemService.findById(1, 2));
-		itemService.updatePrice(1,2,110);
-		System.out.println(itemService.findById(1, 2));
-		itemService.delete(1, 2);
+//		System.out.println(itemService.findById(1, 2));
+//		System.out.println(itemService.findByCanteenId(1));
+//		itemService.makeItemUnavailable(1, 2);
+//		System.out.println(itemService.findById(1, 2));
+//		itemService.makeItemAvailable(1, 2);
+//		System.out.println(itemService.findById(1, 2));
+//		itemService.updatePrice(1,2,110);
+//		System.out.println(itemService.findById(1, 2));
+//		itemService.delete(1, 2);
 
 	}
 
--- CantitoBackend/src/main/java/com/cantito/CantitoBackend/controller/CanteenController.java
+++ CantitoBackend/src/main/java/com/cantito/CantitoBackend/controller/CanteenController.java
@@ -1,11 +1,48 @@
 package com.cantito.CantitoBackend.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.cantito.CantitoBackend.entities.Item;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/canteens")
 public class CanteenController {
 
+    @PostMapping("/additem")
+    public ResponseEntity<Item> addItem(@RequestBody Item item){
+
+    }
+
+    @GetMapping("/{canteenId}/items/{itemId}")
+    public List<Item> getMenu(@PathVariable("canteenId") int canteenId, @PathVariable("itemId")int itemId){
+
+    }
+
+    @GetMapping("/menu/{canteenId}")
+    public List<Item> getMenu(@PathVariable("canteenId") int canteenId){
+
+    }
+
+    @DeleteMapping("/{canteenId}/items/{itemId}")
+    public ResponseEntity<String> deleteItem(@PathVariable("canteenId") int canteenId, @PathVariable("itemId") int itemId){
+
+    }
+
+    @PutMapping("/{canteenId}/items/{itemId}")
+    public ResponseEntity<String> makeItemsAvailable(@PathVariable("canteenId") int canteenId, @PathVariable("itemId") int itemId){
+
+    }
+
+    @PutMapping("/{canteenId}/items/{itemId}")
+    public ResponseEntity<String> makeItemsUnavailable(@PathVariable("canteenId") int canteenId, @PathVariable("itemId") int itemId){
+
+    }
+
+    @PutMapping("/{canteenId}/items/{itemId}/{price}")
+    public ResponseEntity<String> makeItemsUnavailable(@PathVariable("canteenId") int canteenId, @PathVariable("itemId") int itemId, @PathVariable("price")double price){
+
+    }
 
 }
