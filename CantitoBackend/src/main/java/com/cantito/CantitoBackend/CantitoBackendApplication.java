e41a43c77004d4394d1848c0aec76e56a87e703b
kottipranith <saikotti2@gmail.com>
2024-12-24 14:23:15 +0530

    application created

---
+++ CantitoBackend/src/main/java/com/cantito/CantitoBackend/CantitoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.cantito.CantitoBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CantitoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CantitoBackendApplication.class, args);
+	}
+
+}
---
+++ CantitoBackend/src/test/java/com/cantito/CantitoBackend/CantitoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cantito.CantitoBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CantitoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
